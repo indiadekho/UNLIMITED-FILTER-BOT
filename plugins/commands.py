61411b2a650813df98bad39045412a44e2d91da4
indiadekho <73731918+indiadekho@users.noreply.github.com>
2021-06-06 14:10:12 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -243,12 +243,12 @@ async def help(client, message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
+                    InlineKeyboardButton("Need Help?", url="https://telegram.dog/Urs_Bond"),
                     InlineKeyboardButton("About Me", callback_data="about_data")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("Movies Channel", url="https://t.me/HINDI_HD_MOOVIES"),
+                    InlineKeyboardButton(("Movies Group", url="https://t.me/HindiHdMovies_Group")
                 ]
             ]
         ),
@@ -265,7 +265,7 @@ async def about(client, message):
             [
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
+                        "SOURCE CODE", url="https://t.me/Its_My_leech")
                 ],
                 [
                     InlineKeyboardButton("BACK", callback_data="help_data"),
